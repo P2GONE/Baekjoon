49a5ccf313a7490238c7b312aed8fb551b42dffb
JaeHun Gou <106510018+vlrhsgody@users.noreply.github.com>
2024-04-24 03:04:42 +0900

    Add files via upload

--- Silver/Baek_2839.c
+++ Silver/Baek_2839.c
@@ -2,32 +2,48 @@
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
+void print_remain(const int remain, const int remain_3, const int remain_5, const int count) {
+    printf("%d", remain_3 + remain_5 + count - 1);
+}
+
 // % : 나머지, / : 몫
-
 void divis(const int div_num) {
-    int count_5 = 0, count_3 = 0;
-    int remain = div_num; // 남은 수를 초기화
+    int count_5 = 0, count = 0;
+    int tmp_count_5 = 0, tmp_count_3 = 0;
+    int remain = div_num; 
 
-    // 5로 나눌 수 있는만큼 나누고, 남은 수를 3으로 나누어 떨어질 때까지 반복
-    while (remain % 5 != 0 && remain >= 0) {
-        remain -= 3; // 5로 나눌 수 없으면 3을 빼고
-        count_3++;   // 3으로 채움
+    // 예외 처리: 입력이 6 또는 9인 경우
+    if (div_num == 6) {
+        printf("2");
+        return;
     }
-
-    // 나머지가 3으로 나누어 떨어지면
-    if (remain % 3 == 0) {
-        count_3 += remain / 3; // 3으로 나누어 떨어지는 부분을 3으로 채움
-        remain = 0;            // 남은 수 초기화
-    }
-
-    // 남은 수가 0이 아니면 5와 3으로 표현할 수 없음
-    if (remain != 0) {
-        printf("-1\n");
+    if (div_num == 9) {
+        printf("3");
         return;
     }
 
-    // 5와 3으로 표현한 봉지의 개수 출력
-    printf("5의 개수: %d, 3의 개수: %d\n", div_num / 5 - count_5, count_3);
+    // 5로 나눌 수 있는만큼 나누고, 남은 수를 3으로 나누어 떨어질 때까지 반복
+    while (remain > 0) {
+        //5로 나눔
+        if (remain % 5 == 0) {
+            tmp_count_5 = remain / 5;
+        }
+        //3으로 나눔
+        if (remain % 3 == 0) {
+            tmp_count_3 = remain / 3;
+        }
+        count++;
+        if (remain == 3 || remain == 5 || remain == 0) {
+            print_remain(remain, tmp_count_3, tmp_count_5, count);
+            return;
+        }
+        remain = remain - 5;
+    }
+    if (tmp_count_3 > 0 || tmp_count_5 > 0) {
+        print_remain(remain, tmp_count_3, tmp_count_5, count - 1);
+        return;
+    }
+    printf("-1");
 }
 
 int main() {

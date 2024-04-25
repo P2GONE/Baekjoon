2e3bc758aaf67a78031ae87827dbd8f01f3649dd
JaeHun Gou <106510018+vlrhsgody@users.noreply.github.com>
2024-04-25 15:53:42 +0900

    Add files via upload

--- Silver/Baek_2839.c
+++ Silver/Baek_2839.c
@@ -2,48 +2,38 @@
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
-void print_remain(const int remain, const int remain_3, const int remain_5, const int count) {
-    printf("%d", remain_3 + remain_5 + count - 1);
-}
-
+// div_num = 5 *a + 3*b 형식으로 표기함
+// 만약 5*a + 3*b 형식으로 표기되지 않는다면 -1을 출력후 return
+// 입력값이 모두 나누어 떨어진다면 나누어 떨어진 몫을 print함
+// div_num을 3으로 계속 뺀 뒤 나누어 떨어지는지 여부를 확인함
+// 최대한 a가 많이 갖도록해야함 <- 이 문제의 핵심 
 // % : 나머지, / : 몫
 void divis(const int div_num) {
     int count_5 = 0, count = 0;
-    int tmp_count_5 = 0, tmp_count_3 = 0;
     int remain = div_num; 
 
-    // 예외 처리: 입력이 6 또는 9인 경우
-    if (div_num == 6) {
-        printf("2");
-        return;
-    }
-    if (div_num == 9) {
-        printf("3");
-        return;
-    }
-
-    // 5로 나눌 수 있는만큼 나누고, 남은 수를 3으로 나누어 떨어질 때까지 반복
-    while (remain > 0) {
-        //5로 나눔
+    // 3을 계속 빼면서 최대한 5로 나누어떨어질 수 있을 때 까지 반복함
+    while (1) {
+        //5로 우선적으로 나뉠 수 있는지 확인함
         if (remain % 5 == 0) {
-            tmp_count_5 = remain / 5;
+            count_5 = remain / 5;
+            break;
+            //5로 최대한 나뉘어짐 -> 끝
         }
-        //3으로 나눔
-        if (remain % 3 == 0) {
-            tmp_count_3 = remain / 3;
-        }
+        remain = remain - 3;
         count++;
-        if (remain == 3 || remain == 5 || remain == 0) {
-            print_remain(remain, tmp_count_3, tmp_count_5, count);
-            return;
+        if (remain <= 0) {
+            break;
         }
-        remain = remain - 5;
     }
-    if (tmp_count_3 > 0 || tmp_count_5 > 0) {
-        print_remain(remain, tmp_count_3, tmp_count_5, count - 1);
-        return;
+    // 이렇게 하면 나누어 떨어졌을 떄 값을 초기화할 필요가 없음
+    // 그렇다면 여러 케이스가 발생할 수 있는 경우 (a,b) 순서쌍이 여럿 존재할 경우를 무시해도 됨
+    if (remain < 0) {
+        printf("-1");
     }
-    printf("-1");
+    else {
+        printf("%d", count + count_5);
+    }
 }
 
 int main() {

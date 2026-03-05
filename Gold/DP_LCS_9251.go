de3a856a385dc56c962136171afb65374a99c893
JaeHun Gou <charly20@naver.com>
2026-03-06 00:00:50 +0900

    Create DP_LCS_9251.go

---
+++ Gold/DP_LCS_9251.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+// LCS
+func LCS(a, b string) {
+	n := len(a)
+	m := len(b)
+
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+				continue
+			}
+			dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+		}
+	}
+
+	fmt.Println(dp[n][m])
+}
+
+func main() {
+	var a, b string
+	fmt.Fscan(reader, &a, &b)
+
+	LCS(a, b)
+}
